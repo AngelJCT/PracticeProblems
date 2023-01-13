100db863cfc5d4a7f23976bc887982bca0551a29
Parallels <acarrion_pr@yahoo.com>
2023-01-13 19:30:07 -0400

    count negative integers in matrix

---
+++ leetcode_problems/count_negatives_matrix.py
@@ -0,0 +1,10 @@
+# count negatives integers in matrix
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        negatives = 0
+
+        for row in grid:
+            for num in row:
+                if num < 0:
+                    negatives += 1
+        return negatives
