8272cca9cb02b79124a53588c4e1576c91f6587e
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-01 15:46:57 -0400

    optimizing code

--- geeksforgeeks/leaders_in_array.py
+++ geeksforgeeks/leaders_in_array.py
@@ -2,12 +2,10 @@ class Solution:
     #Function to find the leaders in the array.
     def leaders(self, A, N):
         #Code here
-        leaders = []
-        max_so_far = A[-1]
-        for i in range(N - 2, -1, -1):
-            if A[i] >= max_so_far:
-                leaders.append(A[i])
-                max_so_far = A[i]
-        leaders.reverse()
-        leaders.append(A[-1])
-        return leaders
+        leaders = [A[-1]]  # add the last element(rightmost element) in the inialization
+        max_so_far = A[-1]  # initialize the max_so_far with the last element
+        for i in range(N - 2, -1, -1):  # traverse from the right to left from the second last element
+            if A[i] >= max_so_far:  # if the current element is greater or equal than the max_so_far
+                leaders.append(A[i])  # add the current element to the leaders list
+                max_so_far = A[i]  # update the max_so_far
+        return leaders[::-1]  # return the leaders list in reverse order
